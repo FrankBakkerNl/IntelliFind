039fa2aa5dca1e2680ebe573778c8625fb32f60a
Frank Bakker <bakker.frank@gmail.com>
2015-11-18 21:32:31 +0100

    Added some comments to CSharpScript wrapper

--- ScriptWrapper/CSharpScript.cs
+++ ScriptWrapper/CSharpScript.cs
@@ -2,25 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Scripting;
+using MCS = Microsoft.CodeAnalysis.Scripting;
 
 namespace ScriptWrapper
 {
     /// <summary>
-    /// This class wrapps 
+    /// This class wrapps the Microsoft.CodeAnalysis.Scripting API
+    /// This is needed because Visual Studio uses Microsoft.VisualStudio.LanguageServices Version 1.0
+    /// while Microsoft.CodeAnalysis.Scripting requires versio 1.1
     /// </summary>
     public class CSharpScript
     {
         public static Task<T> EvaluateAsync<T>(string code, object globals)
         {
-            var options = ScriptOptions.Default
+            var options = MCS.ScriptOptions.Default
                 .WithReferences(GetReferencedAssemblies())
                 .WithNamespaces(Usings);
 
-            return Microsoft.CodeAnalysis.Scripting.CSharp.CSharpScript.EvaluateAsync<T>(code, options, globals);
+            return MCS.CSharp.CSharpScript.EvaluateAsync<T>(code, options, globals);
         }
 
         private static IEnumerable<Assembly> GetReferencedAssemblies()
@@ -28,9 +28,8 @@ namespace ScriptWrapper
             // Reference all Assemblies currently loaded in the running AppDomain
             return AppDomain.CurrentDomain.GetAssemblies()
                 .Where(a => !a.IsDynamic && !string.IsNullOrEmpty(a.Location))
-                // If versions of the same assembly are loaded we only reference the one with the lowest version
-                // This is neede because currently we use two versions of Microsoft.VisualStudio.LanguageServices
-
+                // HACK! If multiple versions of the same assembly are loaded we only reference the one with the lowest version
+                // This is needed because currently we use two versions of Microsoft.VisualStudio.LanguageServices
                 .GroupBy(a => a.GetName().Name).Select(g => g.OrderBy(a => a.GetName().Version).First());
         }
 
